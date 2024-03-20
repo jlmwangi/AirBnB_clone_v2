f2ad50be14d2c368921c0c4a8876bd2009b80ed5
mwangii <jlmwangi254@gmail.com>
2024-03-20 13:30:53 +0300

    prepare mysql server

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- prepares a mysql server for the project
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db CHARACTER SET utf8mb4;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
