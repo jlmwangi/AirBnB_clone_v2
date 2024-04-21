a2152c2bca4ddc42f2eca9f3dec644b32337ae95
mwangii <jlmwangi254@gmail.com>
2024-04-21 16:59:18 +0300

    check is cool

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -12,5 +12,18 @@ def index():
     return "Hello HBNB!"
 
 
+@app.route('/hbnb', strict_slashes=False)
+def display():
+    """display HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def textc(text):
+    """displays c followed by text"""
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
